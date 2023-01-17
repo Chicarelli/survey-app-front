c84283cb8de73f376bd84502530960b165442887
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2023-01-16 21:36:03 -0300

    feat: add custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      blue: {
+        primary: "#386374",
+        secondary: "#89A3A6"
+      },
+      gray: {
+        primary: "#8B9EA8"
+      },
+      white: {
+        primary: "#F4F4F0"
+      },
+      salmon: {
+        primary: "#B7746F",
+        secondary: "#E2C7BF"
+      }
+    }
   },
   plugins: [],
 }
