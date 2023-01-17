5c114be11a3499b21491216d3893ae744fa7fc8a
Rafael Chicarelli Lopes <rafaelsep20@gmail.com>
2023-01-16 23:54:38 -0300

    config: deleting custom themes applied to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,29 +6,8 @@ module.exports = {
     ".src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+    extend: {
     },
-    colors: {
-      blue: {
-        primary: "#386374",
-        secondary: "#89A3A6"
-      },
-      gray: {
-        primary: "#8B9EA8"
-      },
-      white: {
-        primary: "#F4F4F0"
-      },
-      salmon: {
-        primary: "#B7746F",
-        secondary: "#E2C7BF"
-      }
-    }
   },
   plugins: [],
 }
